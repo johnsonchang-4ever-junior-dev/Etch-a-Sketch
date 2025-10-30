59598a62a6bd2729ef2a40c810a5f35f00f83478
johnsonchang-4ever-junior-dev <johnsonchang0728@gmail.com>
2025-10-30 19:02:52 +1100

    set up all the  elements needed in html files

---
+++ script.js
@@ -0,0 +1,15 @@
+
+
+//createElement()
+//appendChild()
+// select all: querySelectorAll()
+// parseInt() change to integer, prompt() return str
+//check if a class exists → .contains()
+// check if a class exists → .contains()
+// add a class → .add()
+// add id, square.id = 'grid'; or setAttributes('id', ID_1)
+// remove a class → .remove()
+// toggle a class → .toggle() add if missing, rm if exist 
+
+//event.target
+// 
